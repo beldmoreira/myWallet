570deec6cfa9d06f0faca08f92722a912b70918a
Isabela <isabela_cmoreira@hotmail.com>
2022-08-26 17:02:49 -0300

    feat: db connection

--- back/src/db.js
+++ back/src/db.js
@@ -1,9 +1,18 @@
-import { MongoClient } from 'mongodb';
-import dotenv from 'dotenv';
-dotenv.config()
+import {MongoClient} from "mongodb";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+let db = null;
 const mongoClient = new MongoClient(process.env.MONGO_URI);
-await mongoClient.connect();
 
-const db = mongoClient.db("mywallet");
+try {
+  await mongoClient.connect();
+  db = mongoClient.db(process.env.DATABASE);
+  console.log("MongoDB database connected.")
+} catch (error) {
+  console.log("Erro connecting to database.");
+  console.log(error);
+}
+
 export default db;
--- src/schemas/entriesSchema.js
+++
@@ -1,9 +0,0 @@
-import joi from "joi";
-
-const entriesSchema = joi.object({
-    value:joi.number().required(),
-    description: joi.string().required(),
-    type: joi.string().required()
-});
-
-export default entriesSchema;
